82bc1c4c53686b4614a5f728aa49a9775fcecf33
Camille Brulotte <camille.brulotte@icloud.com>
2021-08-19 14:46:45 -0400

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/not_supported_anymore_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.not_supported_anymore_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: new ThemeData(scaffoldBackgroundColor: const Color(0xffef3e45)),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+            child: Text(
+      "Test",
+      style: TextStyle(
+          fontSize: 18.0, fontWeight: FontWeight.bold, color: Colors.white),
+    )));
+  }
+}
